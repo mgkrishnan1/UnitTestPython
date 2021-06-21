0496004a65cf4f12274523e09069e1f4c52b98e6
mgkrishnan1 <80957135+mgkrishnan1@users.noreply.github.com>
2021-06-21 11:55:53 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\40010046\AppData\Local\Programs\Python\Python37-32\python.exe" "D:\Python\UnitTestWithCoverage\Test5SampleClassAdvanced2\test_CheckClass.py"
+"C:\Users\40010046\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\40010046\.jenkins\workspace\PythonUnitTest\Trial1\test_CheckClass.py"
 pause
