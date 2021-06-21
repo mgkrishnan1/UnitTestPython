b4f3f31d1a6fe6a833958c093a17dbb36f42c540
mgkrishnan1 <80957135+mgkrishnan1@users.noreply.github.com>
2021-06-21 11:58:53 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @echo off
 "C:\Users\40010046\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\40010046\.jenkins\workspace\PythonUnitTest\Trial1\test_CheckClass.py"
+coverage run test_CheckClass.py
+coverage html CheckClass.py
+cd D:\Trial\UnitTestTrial\Test6SampleClassAdvancedSetupTearDown\htmlcov
+start chrome.exe D:\Trial\UnitTestTrial\Test6SampleClassAdvancedSetupTearDown\htmlcov\index.html
+pause
 pause
